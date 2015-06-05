9859881caeed25a89e82bc13d8996984e5dcd4e4
shell1026 <mretson0@chicagobooth.edu>
2015-06-05 00:31:32 -0500

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+has_many :bids
+has_many :items, through: :bids
 end
