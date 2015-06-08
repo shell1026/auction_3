85446d2f29e58582c307ee9d8860b5a1fa775156
shell1026 <mretson0@chicagobooth.edu>
2015-06-08 16:29:04 -0500

    incr fixed

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Item do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :item_name, :description, :value, :starting_bid, :increment, :donor, :photo, :category
+  permit_params :item_name, :description, :value, :starting_bid, :incr, :donor, :photo, :category
   #
   # or
   #
