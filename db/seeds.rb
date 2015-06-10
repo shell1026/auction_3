55dbaf5c8b39f4f1efd486f5a2fbf9ecbfd9ad02
shell1026 <mretson0@chicagobooth.edu>
2015-06-10 12:34:39 -0500

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-user = User.create(email: "michelle@example.com", password: "password", first_name: "Michelle", last_name: "Retson", address: "123 Fake St.", city: "Chicago", state: "IL", zip: "60614", company: "HHH", phone: "555-555-5555")
+user = User.create(email: "michelle@example.com", password: "password", first_name: "Michelle", last_name: "Retson", address: "8053 Cleveland Pl", city: "Merrillville", state: "IN", zip: "46410", company: "HHH", phone: "773-669-4673")
 
 humidor = Item.create(
   {
     item_name: "Humidor",
-    description: "This is a great smoking devise.",
+    description: "Cedar humidor, pre-seasoned and loaded with a BONUS GIFT: 6 Cuban cigars brought back personally from Cuba by Dr. Retson.",
     value: 40,
-    starting_bid: 20,
-    incr: 1,
-    donor: "Ronald McDonald",
+    starting_bid: 40,
+    incr: 10,
+    donor: "Healing, Health, & Hope",
     photo: "humidor.jpg",
     category: "Other"
   }
@@ -15,14 +15,27 @@ humidor = Item.create(
 
 buckle = Item.create(
   {
-    item_name: "Buckle",
-    description: "Buckle it up.",
-    value: 20,
-    starting_bid: 10,
-    incr: 1,
-    donor: "Ryan Francis",
+    item_name: "Diamond/Rhodium Bracelet",
+    description: "Rhodium Plated Diamond Accent & Black Enamel Belt Buckle Bangle Bracelet: SilverSpeck Metal: Brass, Material :  Enamel, Gem Type: Diamond, Setting: Pave setting, Width: 12 mm,  Length : 7 in, Clasp: Box with tongue and safety, Stone Weight: 0.01 carats ",
+    value: 80,
+    starting_bid: 20,
+    incr: 10,
+    donor: "Groupon, Inc.",
     photo: "buckle.jpg",
-    category: "Other"
+    category: "Jewelry"
+  }
+)
+
+buckle = Item.create(
+  {
+    item_name: "Car Care Basket",
+    description: "$25 Gift Card for Advanced Auto Parts, various car cleaning items",
+    value: 40,
+    starting_bid: 5,
+    incr: 5,
+    donor: "Advanced Auto Parts",
+    photo: "carcare.jpg",
+    category: "Gift Cards"
   }
 )
 
@@ -30,7 +43,7 @@ humidor_bid = Bid.create(
   {
     item_id: humidor.id,
     user_id: user.id,
-    bid_amount: 25
+    bid_amount: 40
   }
 )
 
@@ -38,6 +51,6 @@ buckle_bid = Bid.create(
   {
     item_id: buckle.id,
     user_id: user.id,
-    bid_amount: 10
+    bid_amount: 20
   }
 )
